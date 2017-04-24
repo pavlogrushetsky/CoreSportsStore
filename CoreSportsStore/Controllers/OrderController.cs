baea6f7244324d0fa26d74dca31b30e1e9e3f433
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2017-04-24 16:27:54 +0300

    Implemented Order form

---
+++ CoreSportsStore/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using CoreSportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreSportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
