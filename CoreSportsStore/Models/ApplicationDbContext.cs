860c1860b12fb1d97b87007e49997b75988be175
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2017-04-24 17:42:58 +0300

    Started to implement Orders repository

--- CoreSportsStore/Models/ApplicationDbContext.cs
+++ CoreSportsStore/Models/ApplicationDbContext.cs
@@ -9,5 +9,7 @@ namespace CoreSportsStore.Models
         }
 
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
---
+++ CoreSportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CoreSportsStore.Models
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> Orders { get; set; }
+        void SaveOrder(Order order);
+    }
+}
